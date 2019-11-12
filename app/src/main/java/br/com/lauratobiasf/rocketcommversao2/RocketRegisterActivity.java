2810c5d1b9cc246c2c69a51deb107048b2b4aa1a
Lydia Garcia <lydiagarcia.chan@gmail.com>
2019-11-12 15:45:25 -0300

    Tela de registro de foguetes

---
+++ app/src/main/java/br/com/lauratobiasf/rocketcommversao2/RocketRegisterActivity.java
@@ -0,0 +1,13 @@
+package br.com.lauratobiasf.rocketcommversao2;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RocketRegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rocket_register);
+    }
+}
